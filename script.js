538ebb6489d98221f010373daca975cbca40de06
AngelHrt <saiber798@gmail.com>
2024-06-10 22:42:35 -0500

    validacion completada

--- script.js
+++ script.js
@@ -4,26 +4,64 @@ function validarFormulario(e) {
     e.preventDefault();
 
     const usuario = document.getElementById('usuario').value;
+    const usuarioError = document.getElementById('usuarioError');
+
+    const nombre = document.getElementById('nombre').value;
+    const nombreError = document.getElementById('nombreError');
+
+    const contrasena = document.getElementById('contrasena').value;
+    const contrasenaError = document.getElementById('contrasenaError');
+
+    const repetirContrasena = document.getElementById('repetirContrasena').value;
+    const repetirContrasenaError = document.getElementById('repetirContrasenaError');
 
     let esValido = true;
 
 
+    const usuarioRegex = /^[a-zA-Z0-9_]{4,16}$/;
+    const nombreRegex = /^[a-zA-Z]+$/;
+    const passRegex = /^[a-zA-Z0-9]{8,16}$/;
 
-    const usuarioRegex = /^[a-zA-Z0-9_]{4,16}$/
+    usuarioError.textContent = '';
+    nombreError.textContent = '';
+    contrasenaError.textContent = '';
+    repetirContrasenaError.textContent = '';
 
+
+    // Validar Usuario
     if(usuario === ''){
-        usuarioError.textContent ='el nombre de usuario vacio es obligatorio';
+        usuarioError.textContent ='El nombre de usuario vacio es obligatorio';
         esValido = false;
     }else if(!usuarioRegex.test(usuario)){
-        usuarioError.textContent = 'El nombre de usuario debe tener entre 4 y 16 digitos y solo pudee contener números y guion bajo.';
+        usuarioError.textContent = 'El nombre de usuario debe tener entre 4 y 16 digitos y solo puede contener letras, números y guion bajo.';
+        esValido = false;
+    }
+    
+    // Validar Nombre
+    if(nombre === ''){
+        nombreError.textContent ='El nombre vacio es obligatorio';
+        esValido = false;
+    }else if(!nombreRegex.test(nombre)){
+        nombreError.textContent = 'El nombre solo puede contener letras.';
         esValido = false;
     }
 
-
-
-    if (esValido == true) {
-    alert('formulario valido')
+    // Validar Contraseña
+    if(contrasena === ''){
+        contrasenaError.textContent ='La contraseña es obligatoria, completar';
+        esValido = false;
+    }else if(!passRegex.test(contrasena)){
+        contrasenaError.textContent = 'La contraseña solo acepta de 8 a 16 caracteres y debe contener números y letras.';
+        esValido = false;
     }
 
-    console.log('hola');
+    //  Validar igualdad de Contraseñas
+    if(contrasena !== repetirContrasena){
+        repetirContrasenaError.textContent ='las contraseñas son distintas';
+        esValido = false;
+    }
+
+    if (esValido == true) {
+        alert('formulario valido')
+    }
 }
